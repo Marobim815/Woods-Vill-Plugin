8f2c78115c2793a82c0e3d1cd4499833b2418f1f
Marobim815 <you@example.com>
2025-01-29 11:28:03 +0900

    개발 시작!!

--- src/main/kotlin/com/github/io/marobim815/woodsVill/core/CoreManager.kt
+++ src/main/kotlin/com/github/io/marobim815/woodsVill/core/CoreManager.kt
@@ -1,7 +1,10 @@
 package com.github.io.marobim815.woodsVill.core
 
 import com.github.io.marobim815.woodsVill.village.VillageManager
+import org.bukkit.Location
 import org.bukkit.Material
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Interaction
 import org.bukkit.entity.Player
 
 object CoreManager {
@@ -14,4 +17,17 @@ object CoreManager {
             else -> null
         }
     }
+
+    fun spawnCoreEntity(player: Player, location: Location) {
+        val world = location.world ?: return  // 월드가 null이면 종료
+        if (!VillageManager.isPlayerInVillage(player) && !VillageManager.isLeader(player)) return
+        val interaction = world.spawnEntity(location, EntityType.INTERACTION) as Interaction
+        val playerVillage = VillageManager.getVillageByPlayer(player)
+        // 엔티티 커스터마이징
+        interaction.isInvulnerable = true  // 코어 엔티티 무적 상태
+        interaction.customName = "${playerVillage?.villageName}마을의 심장"  // 엔티티 이름 설정
+        interaction.isCustomNameVisible = true  // 이름이 보이도록 설정
+
+    }
+
 }
--- src/main/kotlin/com/github/io/marobim815/woodsVill/village/VillageManager.kt
+++ src/main/kotlin/com/github/io/marobim815/woodsVill/village/VillageManager.kt
@@ -1,28 +1,33 @@
 package com.github.io.marobim815.woodsVill.village
 
-import com.sun.jdi.Location
+import org.bukkit.Location
 import org.bukkit.entity.Player
 
 object VillageManager {
-    val villages = mutableListOf<Village?>()
+    val villages = mutableListOf<Village>()
 
     fun addVillage(leader: Player, villageName: String): Boolean {
-        if (villages.any { it?.leader == leader } && villages.any { it?.villageName == villageName }) return false
+        // 중복 검사
+        if (villages.any { it.leader == leader || it.villageName == villageName }) return false
+
         villages.add(Village(leader = leader, villageName = villageName))
         return true
     }
 
-    fun isPlayerInVillage(player: Player): Boolean = villages.any { it?.members?.contains(player)!! }
+    fun isPlayerInVillage(player: Player): Boolean =
+        getVillageByPlayer(player) != null  // 중복 로직 제거
 
-    fun getVillageByLeader(leader: Player): Village? = villages.find { it?.leader == leader }
+    fun getVillageByLeader(leader: Player): Village? =
+        villages.find { it.leader == leader }
 
-    fun getVillageByPlayer(player: Player): Village? = villages.find { it?.members?.contains(player)!! }
+    fun getVillageByPlayer(player: Player): Village? =
+        villages.find { it.members.contains(player) }
 
-    fun isLeader(player: Player): Boolean = villages.any { it?.leader == player }
+    fun isLeader(player: Player): Boolean =
+        villages.any { it.leader == player }
 
     fun addVillageLocation(leader: Player, location: Location) {
         val village = getVillageByLeader(leader)
         village?.coreLocation?.add(location)
     }
-
 }
